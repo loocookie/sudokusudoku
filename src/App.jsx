4e6145c647cd270399e9083610a381fd8e038173
loocookie <138028741+loocookie@users.noreply.github.com>
2025-03-08 02:57:50 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const partition = [
@@ -15,15 +15,70 @@ const partition = [
 
 function App() {
   const [hoveredCell, setHoveredCell] = useState({ row: null, col: null, part: null });
+
+  var [values, setValues] = useState([
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""]
+  ])
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (hoveredCell !== null) {
+        if (event.key >= "1" && event.key <= "9"){
+          console.log(event.key)
+          setValues((prevValues) => {
+            const newValues = prevValues.map((row) => [...row]);
+            newValues[hoveredCell.row][hoveredCell.col] = event.key;
+            return newValues;
+          });
+        }
+        else {
+          if (event.key === "Backspace" || event.key === "Delete") {
+            setValues((prevValues) => {
+              const newValues = prevValues.map((row) => [...row]);
+              newValues[hoveredCell.row][hoveredCell.col] = "";
+              return newValues;
+            });
+          }
+        }
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [hoveredCell])
+
   return (
     <>
       <h1>Partition</h1>
       <div className="grid_container" style={{width: "450px", height: "450px", aspectRatio: 1 / 1, display: "grid", gridTemplateRows: "repeat(9, 1fr)", border: "2px solid black"}}>
         {partition.map((row, rowIndex) => (
           <div key={rowIndex} className="grid_row" style={{display: "grid", gridTemplateColumns: "repeat(9, 1fr)"}}>
-            {row.map((cell, columnIndex) => (
-              <div key={`${rowIndex}-${columnIndex}`} className="grid_cell"  onMouseEnter={() => setHoveredCell({ row: rowIndex, column: columnIndex, part: partition[rowIndex][columnIndex]})}  onMouseLeave={() => setHoveredCell(null)} style={{display: "flex", width: "100%", alignItems: "center", justifyContent: "center", boxSizing: "border-box", backgroundColor: (hoveredCell?.row === rowIndex || hoveredCell?.column === columnIndex || hoveredCell?.part === partition[rowIndex][columnIndex]) ? "rgba(0, 0, 0, 0.1)" : "white", borderLeft: (columnIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex - 1]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)", borderRight: (columnIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex + 1]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)", borderTop: (rowIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex - 1][columnIndex]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)", borderBottom: (rowIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex + 1][columnIndex]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)"}}>
-                {cell}
+            {row.map((_, columnIndex) => (
+              <div 
+                  key={`${rowIndex}-${columnIndex}`}
+                  className="grid_cell"
+                  onMouseEnter={() => setHoveredCell({ row: rowIndex, col: columnIndex, part: partition[rowIndex][columnIndex]})}
+                  onMouseLeave={() => setHoveredCell(null)}
+                  style={{
+                    display: "flex",
+                    width: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    boxSizing: "border-box",
+                    backgroundColor: (hoveredCell?.row === rowIndex || hoveredCell?.col === columnIndex || hoveredCell?.part === partition[rowIndex][columnIndex]) ? "rgba(0, 0, 0, 0.1)" : "white",
+                    borderLeft: (columnIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex - 1]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)",
+                    borderRight: (columnIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex + 1]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)", 
+                    borderTop: (rowIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex - 1][columnIndex]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)",
+                    borderBottom: (rowIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex + 1][columnIndex]) ? "2px solid rgba(0, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.2)"}}>
+                {values[rowIndex][columnIndex]}
               </div>
             ))}
           </div>
