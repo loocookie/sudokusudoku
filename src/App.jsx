68370d564e4d8c6708b3c1408bf4b732bc92dcfa
loocookie <138028741+loocookie@users.noreply.github.com>
2025-03-10 18:10:50 +0900

    Visual update

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,13 @@ const problem = [
 function App() {
   const [hoveredCell, setHoveredCell] = useState({row: null, col: null, part: null});
   const [clickedCell, setClickedCell] = useState({row: null, col: null, part: null})
-  const thickBorder = window.matchMedia('(prefers-color-scheme: dark)').matches ? "2px solid rgb(219, 219, 219)" : "2px solid rgb(36, 36, 36)"
-  const thinBorder = window.matchMedia('(prefers-color-scheme: dark)').matches ? "2px solid rgb(108, 108, 108)" : "2px solid rgb(183, 183, 183)"
-  const relatedBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(60, 60, 60)" : "rgb(231, 231, 231)"
+  const thickBorder = window.matchMedia('(prefers-color-scheme: dark)').matches ? "min(0.36vmin, 2px) solid rgb(219, 219, 219)" : "min(0.36vmin, 2px) solid rgb(36, 36, 36)"
+  const thinBorder = window.matchMedia('(prefers-color-scheme: dark)').matches ? "min(0.36vmin, 2px) solid rgb(60, 60, 60)" : "min(0.36vmin, 2px) solid rgb(231, 231, 231)"
+  const relatedBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(84, 84, 84)" : "rgb(207, 207, 207)"
   const defaultBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(36, 36, 36)" : "rgb(255, 255, 255)"
-  const highlightBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(69, 17, 0)" : "rgb(255, 223, 17)"
-  const writtenBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(84, 84, 84)" : "rgb(207, 207, 207)"
+  const highlightBackground = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(37, 59, 99)" : "rgb(255, 223, 17)"
+  const problemFontColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(219, 219, 219)" : "rgb(36, 36, 36)"
+  const playerFontColor = window.matchMedia('(prefers-color-scheme: dark)').matches ? "rgb(219, 219, 120)" : "rgb(90, 90, 150)"
 
   var [values, setValues] = useState(problem.map((row) =>
     row.map((n) => (n === 0 ? "" : `${n}`))
@@ -86,13 +87,14 @@ function App() {
                       alignItems: "center",
                       justifyContent: "center",
                       boxSizing: "border-box",  
-                      backgroundColor: problem[rowIndex][columnIndex] === 0 ? (clickedCell?.row === rowIndex && clickedCell.col === columnIndex) ? highlightBackground : ((hoveredCell?.row === rowIndex || hoveredCell?.col === columnIndex || hoveredCell?.part === partition[rowIndex][columnIndex]) ? relatedBackground : defaultBackground) : writtenBackground,
+                      backgroundColor: (clickedCell?.row === rowIndex && clickedCell.col === columnIndex) ? highlightBackground : ((hoveredCell?.row === rowIndex || hoveredCell?.col === columnIndex || hoveredCell?.part === partition[rowIndex][columnIndex]) ? relatedBackground : defaultBackground),
                       borderLeft: (columnIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex - 1]) ? thickBorder : thinBorder,
                       borderRight: (columnIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex][columnIndex + 1]) ? thickBorder : thinBorder,
                       borderTop: (rowIndex === 0 || partition[rowIndex][columnIndex] !== partition[rowIndex - 1][columnIndex]) ? thickBorder : thinBorder,
                       borderBottom: (rowIndex === 8 || partition[rowIndex][columnIndex] !== partition[rowIndex + 1][columnIndex]) ? thickBorder : thinBorder,
                       fontSize: "min(4.27vmin, 24px)",
-                      fontWeight: "bold"}}>
+                      fontWeight: "bold",
+                      color: problem[rowIndex][columnIndex] === 0 ? playerFontColor : problemFontColor}}>
                   {values[rowIndex][columnIndex]}
                 </div>
               ))}
@@ -114,7 +116,7 @@ function App() {
                     })
                   }
                 }}}
-              style={{padding: "12px", fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
+              style={{padding: "12px", color: playerFontColor, fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
               {index + 1}
             </button>
           ))}
@@ -130,7 +132,7 @@ function App() {
                   })
                 }
               }}}
-            style={{padding: "12px", fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
+            style={{padding: "12px", color: playerFontColor, fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
             ⌫
           </button>
         </div>
