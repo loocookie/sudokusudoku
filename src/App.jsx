36e454cb078f4fa0d9740aea7fa19633bb1c8fd4
loocookie <138028741+loocookie@users.noreply.github.com>
2025-03-11 12:12:35 +0900

    localstorage added

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,22 @@ function App() {
   const highlightBackground = theme === "dark" ? "rgb(37, 59, 99)" : "rgb(255, 223, 17)"
   const problemFontColor = theme === "dark" ? "rgb(219, 219, 219)" : "rgb(36, 36, 36)"
   const playerFontColor = theme === "dark" ? "rgb(219, 219, 120)" : "rgb(90, 90, 150)"
+  
+  if (!localStorage.getItem("0,0")) {
+    problem.map((row, i) => (
+      row.map((n, j) => (
+        localStorage.setItem(`${i},${j}`, n === 0 ? "" : `${n}`)
+      ))
+    ))
+  }
+  
+  var [values, setValues] = useState(
+    [...Array(9)].map((_, i) => (
+      [...Array(9)].map((_, j) => (
+        localStorage.getItem(`${i},${j}`)[0] === "[" ? JSON.parse(localStorage.getItem(`${i},${j}`)) : localStorage.getItem(`${i},${j}`)
+      ))
+    )))
 
-  var [values, setValues] = useState(problem.map((row) =>
-    row.map((n) => (n === 0 ? "" : `${n}`))
-  ))
   var [writeMode, setWriteMode] = useState("normal")
 
   const collide = (() => {
@@ -78,16 +90,23 @@ function App() {
       if (event.key >= "1" && event.key <= "9") {
         if (writeMode === "normal") {
           newValues[clickedCell.row][clickedCell.col] = event.key;
+          localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, event.key)
         } else {
-          if (!Array.isArray(cellValue) && cellValue === "") {
-            newValues[clickedCell.row][clickedCell.col] = Array(9).fill("");
+          if (!Array.isArray(cellValue)) {
+            if (cellValue === "") {
+              newValues[clickedCell.row][clickedCell.col] = Array(9).fill("");
+              newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] = newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] === "" ? event.key : "";
+            }
           }
-          newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] =
-            newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] === "" ? event.key : "";
+          else {
+            newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] = newValues[clickedCell.row][clickedCell.col][parseInt(event.key, 10) - 1] === "" ? event.key : "";
+          }
+          localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, JSON.stringify(newValues[clickedCell.row][clickedCell.col]))
         }
         setValues(newValues);
       } else if (event.key === "Backspace" || event.key === "Delete") {
         newValues[clickedCell.row][clickedCell.col] = "";
+        localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, "")
         setValues(newValues);
       }
     }
@@ -138,7 +157,7 @@ function App() {
                         justifyContent: "center",
                         boxSizing: "border-box",
                         fontSize: "min(1.78vmin, 10px)",
-                        fontWeight: "normal",
+                        fontWeight: "bold",
                         color: playerFontColor,
                         transition: "0.25s"}}>
                       {value}
@@ -186,10 +205,10 @@ function App() {
         </div>
         <div style={{display: "grid", gridTemplateColumns: "2fr 1fr", paddingTop: "20px", gap: "10px"}}>
           <button onClick={() => {writeMode === "normal" ? setWriteMode("candidate") : setWriteMode("normal")}} style={{padding: "12px", color: problemFontColor, fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
-          {writeMode === "normal" ? "Candidate Mode" : "Normal Mode"}
+          {writeMode === "normal" ? "Normal Mode" : "Candidate Mode"}
           </button>
           <button onClick={() => {theme === "light" ? setTheme("dark") : setTheme("light")}} style={{padding: "12px", color: problemFontColor, fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
-            {theme === "light" ? "Dark" : "Light"}
+            {theme === "light" ? "Light" : "Dark"}
           </button>
         </div>
         <div style={{display: "grid", gridTemplateColumns: "repeat(5, 1fr)", paddingTop: "20px", paddingBottom: "20px", gap: "10px"}}>
@@ -205,6 +224,7 @@ function App() {
                         newValues[clickedCell.row][clickedCell.col] = `${index + 1}`;
                         return newValues;
                       });
+                      localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, `${index + 1}`)
                     }
                     else {
                       if (Array.isArray(values[clickedCell.row][clickedCell.col])) {
@@ -212,6 +232,7 @@ function App() {
                           setValues((prevValues) => {
                             const newValues = prevValues.map((row) => [...row]);
                             newValues[clickedCell.row][clickedCell.col][index] = `${index + 1}`;
+                            localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, JSON.stringify(newValues[clickedCell.row][clickedCell.col]))
                             return newValues;
                           });
                         }
@@ -219,6 +240,7 @@ function App() {
                           setValues((prevValues) => {
                             const newValues = prevValues.map((row) => [...row]);
                             newValues[clickedCell.row][clickedCell.col][index] = "";
+                            localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, JSON.stringify(newValues[clickedCell.row][clickedCell.col]))
                             return newValues;
                           });
                         }
@@ -229,6 +251,7 @@ function App() {
                             const newValues = prevValues.map((row) => [...row]);
                             newValues[clickedCell.row][clickedCell.col] = Array(9).fill("");
                             newValues[clickedCell.row][clickedCell.col][index] = `${index + 1}`
+                            localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, JSON.stringify(newValues[clickedCell.row][clickedCell.col]))
                             return newValues;
                           });
                         }
@@ -250,6 +273,7 @@ function App() {
                     newValues[clickedCell.row][clickedCell.col] = ""
                     return newValues
                   })
+                  localStorage.setItem(`${clickedCell.row},${clickedCell.col}`, "")
                 }
               }}}
             style={{padding: "12px", color: playerFontColor, fontSize: "min(3.2vmin, 18px)", fontWeight: "bold", border: thinBorder, background: defaultBackground}}>
